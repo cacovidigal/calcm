22ac6868b55f84ae9998bf1b298d64ffde5e3aa4
cacovidigal <marcosmsvidigal@gmail.com>
2023-05-31 22:17:32 -0300

    entrega do projeto bmi

--- bmi-calculator/starter-code/calculadora.js
+++ bmi-calculator/starter-code/calculadora.js
@@ -5,9 +5,13 @@ function somar(){
     var res = window.document.getElementById('res')
     var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
-    var s = n1 / (n2**2)
+    var s = n1 / (n2**2)*10000
     
-    //res.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${s}`
+    res.innerHTML = ` <p>Your BMI is ${s} look the BMI ranges and their weight classifications. </p>
+    <p>- Underweight: BMI less than 18.5<p>
+    <p> - Healthy weight: BMI 18.5 to 24.9<p>
+    <p>- Overweight: BMI 25 to 29.9<p>
+    <P>- Obese: BMI 30 or greater<p> `
 }
 
 /*Below you will find the BMI ranges and their weight classifications. Based on the person's BMI results, add their weight classification onto sentence "Your BMI suggests you’re" within the results component.
